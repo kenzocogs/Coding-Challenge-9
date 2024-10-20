15c8eea9f13d7f986f2442239ca07eedd013bce9
Kenzo Cogswell <kcogswell@usf.edu>
2024-10-20 18:35:46 -0400

    Create patron class to borrow and return books

--- library_management.js
+++ library_management.js
@@ -8,11 +8,11 @@ constructor (title, author, ISBN, _isAvailable) {
     this._isAvailable = true // default state is true here 
 }
 // getter
-get _isAvailable () {
+get isAvailable () {
     return this._isAvailable
 }
 // setter
-set _isAvailable (status) {
+set isAvailable (status) {
     this._isAvailable = status
 }
 getDetails() {
@@ -34,11 +34,38 @@ addBook(book) {
 }
 
 getAvailableBooks () {
-    this.books.filter (book => book._isAvailable).length // filtering for the available books here
+    this.books.filter (book => book.isAvailable).length // filtering for the available books here
 }
 
 listBooks () {
     this.books.forEach (book =>
-        console.log (`Title: ${book.title}, Available: ${book._isAvailable}`) // only want title and availabillity here. 
+        console.log (`Title: ${book.title}, Available: ${book.isAvailable}`) // only want title and availabillity here. 
     )
 }}
+
+// Task 3: Create a Patron Class
+
+class Patron {
+    constructor (name, borrowedBooks) {
+        name = this.name
+        borrowedBooks = [] // initializing empty array
+    }
+
+borrowBook(book) {
+    if (book instanceof Book && book.isAvailable === true) { //checking that book is available
+        this.borrowedBooks.push (book)
+        book.isAvailable = false
+        console.log(`${book.title} has now been borrowed.`); } // logging this msg after marking book false and adding it as borrowed
+            else console.log (`ERROR: ${book.title} is either invalid or not available`)
+}
+
+returnBook (book) {
+let bookFinder = this.borrowedBooks.find(item = item.title === book.title) // checking that book was borrowed by patron
+    if (book instanceof Book & bookFinder) {
+        this.borrowedBooks.isAvailable = true
+        this.borrowedBooks.filter(b => b !== book) // taking the book out of the array now that it's been returned
+        console.log(`${book.title} has now been returned.`)
+    } else {
+        console.log(`ERROR:${book.title} cannot be returned. Please contact administration about the error.`)
+    }
+}};
